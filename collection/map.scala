df515a35a6eec73b52f0b2b6bc7163043e54d4d7
peke2 <abem@dc4.so-net.ne.jp>
2015-08-07 11:14:00 +0900

    Mapの扱いの確認を追加。

--- collection/map.scala
+++ collection/map.scala
@@ -10,3 +10,29 @@ list.foreach{key_value=>
 	println(key_value._1+"->"+key_value._2)
 }
 
+println()
+println("--------")
+
+//	空のリストを作成
+var array_list = Map.empty[String, Array[String]]
+
+array_list = array_list + ("b"-> Array("aaa","bbb","ccc"))
+array_list = array_list + ("a"-> Array("ddd","fff","ggg"))
+array_list = array_list + ("c"-> Array("iii","hhh","mmm"))
+
+//	そのまま列挙
+println("---- enumerate")
+for(arr <- array_list){
+	val (k, v) = arr
+	println("key["+k+"]->"+v.mkString(","))
+}
+
+
+//	キーで要素を取り出す
+println("---- enumerate by key")
+for(k <- Array("a","b","c")){
+	val v = array_list(k)
+	println("key["+k+"]->"+v.mkString(","))
+}
+
+
