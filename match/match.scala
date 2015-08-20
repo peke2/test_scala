6eb49efc3f68fc1c7dfb528c4a3c138bc7c80961
peke2 <abem@dc4.so-net.ne.jp>
2015-08-20 20:14:41 +0900

    マッチングの正規表現使用確認のパターン追加。

--- match/match.scala
+++ match/match.scala
@@ -1,19 +1,30 @@
 
 //	マッチング
 
-val str = "2000年11月～2001年3月"
+val strList = List(
+	"2000年11月～2001年3月",
+	"2013/2/21 ～",
+	"2013/2/21 ～2015/8/9"
+)
 
 //	StringLikeクラスのメソッド「r」を使うと文字列から正規表現を得られる
 val exp = "([0-9]+)年([0-9]+)月～([0-9]+)年([0-9]+)月".r
+//val exp2 = "([0-9]+)/([0-9]+)/([0-9]+)[~～]?(([0-9]+)/([0-9]+)/([0-9]+))?".r
+val exp2 = "([0-9]+)/([0-9]+)/([0-9]+)～([0-9]+)/([0-9]+)/([0-9]+)".r
+val exp3 = "([0-9]+)/([0-9]+)/([0-9]+)[~～]?".r
 
 
 //	正規表現クラスの説明
 //	日時のパースとマッチングについても書かれているので参考になる
 //	http://www.scala-lang.org/api/current/#scala.util.matching.Regex
 
-str match{
-	case exp(st_y,st_m,ed_y,ed_m) => println(st_y+"/"+st_m+"～"+ed_y+"/"+st_m)
-	//case exp(st_y,st_m,ed_y,ed_m) => println(s"$st_y/$st_m～$ed_y/$st_m")
-	case _ => println("No match")
+for(str <- strList)
+{
+	str.replaceAll("[ 　]","") match{
+		case exp(st_y,st_m,ed_y,ed_m) => println(s"exp=$st_y/$st_m～$ed_y/$ed_m")
+		//case exp(st_y,st_m,ed_y,ed_m) => println(s"$st_y/$st_m～$ed_y/$st_m")
+		case exp2(st_y,st_m,st_d,ed_y,ed_m,ed_d) => println(s"exp2=$st_y/$st_m～$ed_y/$ed_m")
+		case exp3(st_y,st_m,st_d) => println(s"exp3=$st_y/$st_m～$st_y/$st_m")
+		case s => println(s"[$s] is No match")
+	}
 }
-
